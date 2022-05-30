795dafda65059b19240bb1a356b83b02ce20330a
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-05-30 14:35:21 +0200

    added exercises js

---
+++ E4/app.js
@@ -0,0 +1,5 @@
+//Escribe un programa que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+let num1 = prompt("Introduce el primer número: ");
+let num2 = prompt("Introduce el segundo número: ");
+
